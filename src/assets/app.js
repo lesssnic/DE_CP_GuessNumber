32ab4f461b9e56c21aa8436c9f2f466c2af5c38d
lesssnic <antoxxxa.ua@gmail.com>
2021-10-31 13:15:40 +0200

    skeleton added

---
+++ src/assets/app.js
@@ -0,0 +1,7 @@
+
+import * as anton from './segments/anton.js';
+import * as andrew from './segments/andrew.js';
+import * as kate from './segments/kate.js';
+
+kate.getRandomNumber();
+kate.validateNumber();
---
+++ src/assets/segments/kate.js
@@ -0,0 +1,13 @@
+
+export function validateNumber (number) {
+    console.log('validateNumber');
+//    на число, и целое ли оно больше нуля и не больше 200 return Boolean;
+
+}
+
+
+export function getRandomNumber (min, max) {
+    console.log('getRandowmNumber');
+//   if (validatorNumber (min), validatorNumber (max)) {return    генерируем случайное число из диапозона }else return ‘Error’
+
+}
