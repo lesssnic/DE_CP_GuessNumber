caf2652c06218f9be979483d4db3e0dea5ebf746
richerstars <81115334+richerstars@users.noreply.github.com>
2021-11-06 15:51:25 +0200

    switcher UI

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
-
 const startGameButton = document.querySelector('#generator');
 const guessNumberButton = document.querySelector('#guess');
 const resetButton = document.querySelector('#exit');
-    startGameButton.addEventListener('click', startGame);
-    guessNumberButton.addEventListener('click', guessNumber);
-    resetButton.addEventListener('click', reset);
-
-function startGame () {
+startGameButton.addEventListener('click', startGame);
+guessNumberButton.addEventListener('click', guessNumber);
+resetButton.addEventListener('click', reset);
+const hider = document.querySelector('#main_hide');
+const hider2 = document.querySelector('#hiding');
+const min = document.querySelector('#minValue');
+const max = document.querySelector('#maxValue');
+const outputtterError = document.querySelector('#text2');
+function startGame() {
+    if (Number(min.value) >= Number(max.value)) {
+        return outputtterError.innerHTML = 'Ваш диапазон задан неверно!';
+    } else {
+        hider.style.display = 'none';
+        hider2.style.display = 'block';
+    }
+    console.log(Number(min.value), Number(max.value));
     console.log('start');
 }
 
-function guessNumber () {
+function guessNumber() {
     console.log('guess');
 }
 
-function reset () {
+function reset() {
     console.log('reset');
 }
