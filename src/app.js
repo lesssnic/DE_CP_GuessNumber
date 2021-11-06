fc7539128a8c943dd9854ece1be7e3634025af20
richerstars <81115334+richerstars@users.noreply.github.com>
2021-11-06 18:45:44 +0200

    some func

--- src/app.js
+++ src/app.js
@@ -10,23 +10,43 @@ const hider2 = document.querySelector('#hiding');
 const min = document.querySelector('#minValue');
 const max = document.querySelector('#maxValue');
 const outputtterError = document.querySelector('#text2');
+const user = document.querySelector('#userInput');
+
+let randRes = 0;
+let counter = 5;
 
 function startGame() {
-    console.log(Number(min.value), Number(max.value));
-    console.log('start');
     if (Number(min.value) >= Number(max.value) || (min.value < 0 || max.value < 0) || (min.value > 200 || max.value > 200)) {
         return outputtterError.innerHTML = 'Ваш диапазон задан неверно!';
     } else {
         hider.style.display = 'none';
         hider2.style.display = 'block';
-        return getRandomNumber(Number(min.value), Number(max.value));
+        const random = getRandomNumber(Number(min.value), Number(max.value));
+        console.log(random);
+        randRes = random;
     }
-
 }
 
 function guessNumber() {
-
-    console.log('guess');
+    if (counter > 1) {
+        switch (true) {
+            case user.value < randRes:
+                finalOut.innerHTML = "Значение меньше заданного!";
+                break;
+            case user.value > randRes:
+                finalOut.innerHTML = "Значение больше заданного!";
+                break;
+            default:
+                finalOut.innerHTML = "Поздравляю, вы угадали число!";
+                guessNumberButton.disabled = true;
+                break;
+        }
+    } else {
+        finalOut.innerHTML = "Вы проиграли. У вас не осталось попыток!";
+        guessNumberButton.disabled = true;
+    }
+    counter--;
+    user.value = '';
 }
 
 function reset() {
