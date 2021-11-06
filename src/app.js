2adbd9759488ef40e78b110cdcc1f68f21e6fe5c
lesssnic <antoxxxa.ua@gmail.com>
2021-11-06 14:44:52 +0200

    add reset button in js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+
 const startGameButton = document.querySelector('#generator');
 const guessNumberButton = document.querySelector('#guess');
+const resetButton = document.querySelector('exit');
     startGameButton.addEventListener('click', startGame);
     guessNumberButton.addEventListener('click', guessNumber);
+    resetButton.addEventListener('click', reset);
 
 function startGame () {
     console.log('start');
@@ -9,4 +12,8 @@ function startGame () {
 
 function guessNumber () {
     console.log('guess');
+}
+
+function reset () {
+    console.log('reset');
 }
