658ff67aedb3538bdb6ea76f6c668385c4e02555
lesssnic <antoxxxa.ua@gmail.com>
2021-11-19 11:27:53 +0200

    add default value

--- src/helpers.js
+++ src/helpers.js
@@ -1,22 +1,9 @@
 function validateNumber(number) {
-    if (typeof number !== 'number' || number < 0 || number > 200 || !Number.isInteger(number)) {
-        return true;
-    } else {
-        return false;
-    }
+    return (typeof number !== 'number' || number < 0 || number > 200 || !Number.isInteger(number));
 }
 function getRandomNumber(min, max) {
-    if (!(validateNumber(min) && validateNumber(max))) {
-        return Math.floor(Math.random() * (max - min) + min);
-    } else {
-        return null;
-    }
+    return (!(validateNumber(min) && validateNumber(max)))?Math.floor(Math.random() * (max - min) + min):null;
 }
 function createFinishing(element) {
-    if (element == 4 || element == 3 || element == 2) {
-        return 'и';
-    }
-    else {
-        return 'а';
-    }
+    return (element == 4 || element == 3 || element == 2)?'и':'а';
 }
